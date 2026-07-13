d31bfdeaec3fa53c7d68118749616e98532024a4
arifrahman-pixel <arifurahaman2005@gmail.com>
2026-07-13 20:58:44 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Console Quiz Game
+
+A simple, interactive multiple-choice quiz program written in Python. This project demonstrates core Object-Oriented Programming (OOP) concepts, including classes, constructors (`__init__`), and object instantiation.
+
+## How to Run
+Run the main script using Python:
+```bash
+python main.py
---
+++ quiz_runner.py
@@ -0,0 +1,24 @@
+from e1_Questions import Question
+
+Question_prompts = [
+    "Whats the color of apple\n(1) Red\n(2) Blue\n(3) Yellow\n",
+    "Whats the color of banana\n(1) Silver\n(2) Pink\n(3) Yellow\n",
+    "Whats the color of Oranges\n(1) Orange\n(2) Red\n(3) Pink\n",
+    "Whats the color of Strawberry\n(1) Orange\n(2) Red\n(3) Pink\n",
+]
+Questions = [
+    Question(Question_prompts[0],"1"),
+    Question(Question_prompts[1],"3"),
+    Question(Question_prompts[2],"1"),
+    Question(Question_prompts[3],"2"),
+]
+def run_test(Questions):
+    score = 0
+    for Question in Questions:
+        answer = input(Question.prompt)
+        if answer == Question.answer:
+         score += 1
+    print("You got " + str(score)+" out of "+ str(len(Questions)))
+
+run_test(Questions)
+
